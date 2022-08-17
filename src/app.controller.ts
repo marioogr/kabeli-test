201a4ceff3010d12066a0d3f77afc4f9a1781b59
Gabriel Aponte <gabrielaponte020@gmail.com>
2022-08-17 09:38:40 -0400

    DCD-188: modulo de cache con redis

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,42 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  CACHE_MANAGER,
+  CacheInterceptor,
+  CacheKey,
+  CacheTTL,
+  Controller,
+  Get,
+  Inject,
+  UseInterceptors,
+} from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    @Inject(CACHE_MANAGER) private cacheManager: any,
+  ) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('health')
+  health(): string {
+    return 'OK!!';
+  }
+
+  // TODO: Remover esto al iniciar un projecto nuevo, ya que es un ejemplo de como usar redis en un controller
+  @Get('cache-redis')
+  @UseInterceptors(CacheInterceptor)
+  @CacheKey('cacheRedis')
+  @CacheTTL(20)
+  cacheRedis(): Promise<string> {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve('onCache');
+      }, 3000);
+    });
+  }
+
+  @Get('remove-cache')
+  removeCache() {
+    return this.cacheManager.del('cacheRedis');
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,27 @@
-import { Module } from '@nestjs/common';
+import { Module, CacheModule } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import type { ClientOpts } from 'redis';
+import * as redisStore from 'cache-manager-redis-store';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ['.env.local'],
+    }),
+    CacheModule.registerAsync<ClientOpts>({
+      useFactory: async (configService: ConfigService) => ({
+        store: redisStore,
+        host: configService.get('REDIS_HOST'),
+        port: configService.get('REDIS_PORT'),
+        isGlobal: true,
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
